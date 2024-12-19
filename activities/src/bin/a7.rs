b9a9f73727a0d120ef81538095a4adb5afcbd7b0
aeyquem <fabriziorossi@vivaldi.net>
2024-12-19 16:52:19 -0300

    feat: solved a7

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,5 +10,27 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+// * Use an enum with color names as variants
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
 
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+fn print_color(color: Color) {
+    // * Use a match expression to determine which color
+    //   name to print
+    match color {
+        Color::Blue => println!("blue"),
+        Color::Green => println!("green"),
+        Color::Red => println!("red"),
+    }
+}
+
+fn main() {
+    let color = Color::Blue;
+
+    print_color(color);
+}
