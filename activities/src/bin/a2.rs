df88e8738b8e07a263ec5686c1b05cbbb68a3f60
aeyquem <fabriziorossi@vivaldi.net>
2024-12-19 16:17:19 -0300

    feat: solved a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,5 +8,21 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    let a = 0;
+    let b = 10;
+    // * Use a function to add two numbers together
+    let result = sum_two_nums(a, b);
 
+    // * Use a function to display the result
+    print_result(result);
+}
+
+fn print_result(result: i32) {
+    // * Use the "{:?}" token in the println macro to display the result
+    println!("result is {:?}", result);
+}
+
+fn sum_two_nums(a: i32, b: i32) -> i32 {
+    return a + b;
+}
