a0a531fcbbdf480431efbd35323ece4cfea88478
aeyquem <fabriziorossi@vivaldi.net>
2024-12-24 13:09:59 -0300

    feat: solved a12

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -10,5 +10,45 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+// * Use a struct to encapsulate the box characteristics
+struct Box {
+    color: Color,
+    weight: i32,
+    height: i32,
+    width: i32,
+    length: i32,
+}
 
+// * Use an enum for the box color
+#[derive(Debug)]
+enum Color {
+    Brown,
+}
+
+impl Box {
+    // * Implement functionality on the box struct to create a new box
+    fn create_box(color: Color, weight: i32, height: i32, width: i32, length: i32) -> Box {
+        let new_box = Box {
+            color,
+            weight,
+            height,
+            width,
+            length,
+        };
+
+        return new_box;
+    }
+    // * Implement functionality on the box struct to print the characteristics
+    fn print_data(&self) {
+        println!("color: {:?}", self.color);
+        println!("weight: {:?}", self.weight);
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("length: {:?}", self.length);
+    }
+}
+
+fn main() {
+    let my_box = Box::create_box(Color::Brown, 13, 10, 5, 1);
+    my_box.print_data();
+}
